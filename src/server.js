fd1f3f9597b017b7a5b70473f243e422c15562ee
dk.davidson <13313@coderadademy.edu.au>
2023-08-04 06:05:20 +1000

    build: database connected

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+const url = 'mongodb://127.0.0.1:27017';
 
 app.use(express.json());
+
+async function dbConnect() {
+  try {
+    // await mongoose.connect("mongodb://127.0.0.1:27017/REmind_m3_db");
+    await mongoose.connect("mongodb+srv://13313:TTKp70e5DQwkkS36@cluster0.9gl1zri.mongodb.net/");
+    console.log("Database Connected!");
+  } catch (error) {
+    console.log(`dbConnect failed, error:${JSON.stringify(error)}`);
+  }
+}
+
+dbConnect();
 
 app.get("/", (req, res) => {
   res.json({
@@ -10,7 +23,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const eventsRouter = require("./routes/events_routes")
+const eventsRouter = require("./routes/events_routes");
 app.use("/events", eventsRouter);
 
 module.exports = {
