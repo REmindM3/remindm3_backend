d2af036efc39c85859195ac8a437452b57c0c01d
dk.davidson <13313@coderadademy.edu.au>
2023-08-04 07:28:00 +1000

    build: create, read & delete working in db

--- src/controllers/event_controller.js
+++ src/controllers/event_controller.js
@@ -1,22 +1,31 @@
-const Event = require("../models/events")
+const Event = require("../models/events");
 
-const getEvents = (req, res) => {
-    res.json({ message: "The list of events is here" });
-  }
+const getEvents = async (req, res) => {
+  let events = await Event.find();
+  res.send(events);
+};
 
-const createEvent = async(req, res) => {
-    let newEvent = new Event({
-        title: req.body.title,
-        description: req.body.description, 
-        isActive: true,
-        isPrivate: true, 
-        alertDate: new Date().toLocaleDateString(), 
-        createdAtDate: Date.now(),
-    })
-    await newEvent.save()
-    res.json({
-        event: newEvent
-    })
-}
- 
-module.exports = {getEvents, createEvent}
+const createEvent = async (req, res) => {
+  let newEvent = new Event({
+    title: req.body.title,
+    description: req.body.description,
+    isActive: true,
+    isPrivate: true,
+    alertDate: new Date().toLocaleDateString(),
+    createdAtDate: Date.now(),
+  });
+  await newEvent.save();
+  res.status(201);
+  res.json({
+    event: newEvent,
+  });
+};
+
+const deleteAllEvents = async (req, res) => {
+  await Event.deleteMany({});
+  res.json({
+    message: "All Notes Deleted",
+  });
+};
+
+module.exports = { getEvents, createEvent, deleteAllEvents };
--- src/routes/events_routes.js
+++ src/routes/events_routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const eventsRouter = express.Router();
-const {getEvents, createEvent} = require("../controllers/event_controller");
+const {getEvents, createEvent, deleteAllEvents} = require("../controllers/event_controller");
 
 
 eventsRouter.get("/", getEvents);
 
 eventsRouter.post("/", createEvent);
+
+eventsRouter.delete("/clear", deleteAllEvents);
 
 module.exports = eventsRouter
